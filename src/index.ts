30006cc4c19f6006e090a58356598d489804a8ab
Mifaki <agustianto.d19@gmail.com>
2026-05-15 11:42:03 +0700

    build: trigger new build cache

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ registerModule(app);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on <http://localhost:${PORT}> 🚀`);
+  console.log(`Server is running on http://localhost:${PORT}>🚀`);
 });
 
 export default app;
